b1c7286b251d61bb90407d75860fba7728ae8fde
Igor Korsukov <igor.korsukov@gmail.com>
2024-03-28 14:51:35 +0200

    added network as is

---
+++ framework/network/inetworkmanager.h
@@ -0,0 +1,56 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2021 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef MU_NETWORK_INETWORKMANAGER_H
+#define MU_NETWORK_INETWORKMANAGER_H
+
+#include "types/ret.h"
+#include "async/channel.h"
+#include "global/progress.h"
+#include "networktypes.h"
+
+class QUrl;
+
+namespace mu::network {
+class INetworkManager
+{
+public:
+    virtual ~INetworkManager() = default;
+
+    virtual Ret get(const QUrl& url, IncomingDevice* incomingData, const RequestHeaders& headers = RequestHeaders()) = 0;
+    virtual Ret head(const QUrl& url, const RequestHeaders& headers = RequestHeaders()) = 0;
+    virtual Ret post(const QUrl& url, OutgoingDevice* outgoingData, IncomingDevice* incomingData,
+                     const RequestHeaders& headers = RequestHeaders()) = 0;
+    virtual Ret put(const QUrl& url, OutgoingDevice* outgoingData, IncomingDevice* incomingData,
+                    const RequestHeaders& headers = RequestHeaders()) = 0;
+    virtual Ret patch(const QUrl& url, OutgoingDevice* outgoingData, IncomingDevice* incomingData,
+                      const RequestHeaders& headers = RequestHeaders()) = 0;
+    virtual Ret del(const QUrl& url, IncomingDevice* incomingData, const RequestHeaders& headers = RequestHeaders()) = 0;
+
+    virtual mu::Progress progress() const = 0;
+
+    virtual void abort() = 0;
+};
+
+using INetworkManagerPtr = std::shared_ptr<INetworkManager>;
+}
+
+#endif // MU_NETWORK_INETWORKMANAGER_H
---
+++ framework/network/internal/networkmanager.h
@@ -0,0 +1,90 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2021 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef MU_NETWORK_NETWORKMANAGER_H
+#define MU_NETWORK_NETWORKMANAGER_H
+
+#include "inetworkmanager.h"
+
+class QNetworkAccessManager;
+class QNetworkRequest;
+class QNetworkReply;
+
+namespace mu::network {
+class NetworkManager : public QObject, public INetworkManager
+{
+    Q_OBJECT
+
+public:
+    explicit NetworkManager(QObject* parent = nullptr);
+    ~NetworkManager() override;
+
+    Ret get(const QUrl& url, IncomingDevice* incomingData, const RequestHeaders& headers = RequestHeaders()) override;
+    Ret head(const QUrl& url, const RequestHeaders& headers = RequestHeaders()) override;
+    Ret post(const QUrl& url, OutgoingDevice* outgoingData, IncomingDevice* incomingData,
+             const RequestHeaders& headers = RequestHeaders()) override;
+    Ret put(const QUrl& url, OutgoingDevice* outgoingData, IncomingDevice* incomingData,
+            const RequestHeaders& headers = RequestHeaders()) override;
+    Ret patch(const QUrl& url, OutgoingDevice* outgoingData, IncomingDevice* incomingData,
+              const RequestHeaders& headers = RequestHeaders()) override;
+    Ret del(const QUrl& url, IncomingDevice* incomingData, const RequestHeaders& headers = RequestHeaders()) override;
+
+    mu::Progress progress() const override;
+
+    void abort() override;
+
+private:
+    enum RequestType {
+        GET_REQUEST,
+        HEAD_REQUEST,
+        POST_REQUEST,
+        PUT_REQUEST,
+        PATCH_REQUEST,
+        DELETE_REQUEST
+    };
+
+    Ret execRequest(RequestType requestType, const QUrl& url, IncomingDevice* incomingData = nullptr,
+                    OutgoingDevice* outgoingData = nullptr, const RequestHeaders& headers = RequestHeaders());
+
+    QNetworkReply* receiveReply(RequestType requestType, const QNetworkRequest& request, OutgoingDevice* outgoingData = nullptr);
+
+    bool openDevice(QIODevice* device, QIODevice::OpenModeFlag flags);
+    void closeDevice(QIODevice* device);
+
+    bool isAborted() const;
+
+    void prepareReplyReceive(QNetworkReply* reply, IncomingDevice* incomingData);
+    void prepareReplyTransmit(QNetworkReply* reply);
+
+    Ret waitForReplyFinished(QNetworkReply* reply, int timeoutMs);
+    Ret errorFromReply(const QNetworkReply* reply) const;
+
+private:
+    QNetworkAccessManager* m_manager = nullptr;
+    IncomingDevice* m_incomingData = nullptr;
+    QNetworkReply* m_reply = nullptr;
+    mu::Progress m_progress;
+
+    bool m_isAborted = false;
+};
+}
+
+#endif // MU_NETWORK_NETWORKMANAGER_H
